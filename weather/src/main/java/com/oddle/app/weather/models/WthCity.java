28cddc4e3fc7c1513abe00299e65ac98e02c8420
Yusuf Habibi <yusuf.habibi@next-ti.co.id>
2021-03-13 02:19:00 +0700

    add model wthCity & wthCoordinate

---
+++ weather/src/main/java/com/oddle/app/weather/models/WthCity.java
@@ -0,0 +1,40 @@
+package com.oddle.app.weather.models;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "wth_city")
+public class WthCity implements Serializable {
+
+    private static final long serialVersionUID = -8546777162330650796L;
+   
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) 
+    @Column(name = "id", nullable = true)
+    private long id;
+    
+    @Column(name = "name", nullable = true)
+    @NotEmpty(message="* Please Enter City Name")
+    private String name;
+
+    @Column(name = "state", nullable = true)
+    @NotEmpty(message="* Please Enter State Name")
+    private String state;
+
+    @Column(name = "country", nullable = true)
+    @NotEmpty(message="* Please Enter Coutry Name")
+    private String country;
+
+    
+}
