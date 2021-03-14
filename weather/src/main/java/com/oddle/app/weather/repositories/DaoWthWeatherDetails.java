e2458bf585083cd4b4ac374ffd62162ef8546bf9
Yusuf Habibi <yusufhabibi1998@gmail.com>
2021-03-14 15:52:48 +0700

    create new query

--- weather/src/main/java/com/oddle/app/weather/repositories/DaoWthWeatherDetails.java
+++ weather/src/main/java/com/oddle/app/weather/repositories/DaoWthWeatherDetails.java
@@ -1,12 +1,31 @@
 package com.oddle.app.weather.repositories;
 
-import com.oddle.app.weather.models.WthCity;
+import java.util.Date;
+import java.util.List;
+
 import com.oddle.app.weather.models.WthWeatherDetails;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface DaoWthWeatherDetails extends JpaRepository<WthWeatherDetails,Long> {
-	WthCity findById(long id);    
+	WthWeatherDetails findById(long id);    
+	//List<WthWeatherDetails> findByCityIdAndWeatherDtAfterAndWeatherDtBefore(Long cityId, Date endDt, Date fromDt);
+	
+
+	@Query( value= "\n select *                        "
+	             + "\n  from wth_weather_details wtd   "
+	             + "\n  where 1=1                      "                                               
+				 + "\n  and wtd.city_id = :cityId      "
+				 + "\n  and (wtd.weather_dt between DATE(:fromDt) and DATE(:endDt))"
+				 + "\n  order by wtd.weather_dt desc   "
+	, nativeQuery = true)
+
+List<WthWeatherDetails> findByCityIdAndRangeDt(@Param("cityId") long cityId
+											 , @Param("fromDt") Date fromDt
+											 , @Param("endDt")  Date endDt);
+
 }
