b6ea882cf1268844c41357b7c26e186d3715f63d
Yusuf Habibi <yusufhabibi1998@gmail.com>
2021-03-14 15:52:32 +0700

    change type Date -> LocalDate for weatherDt

--- weather/src/main/java/com/oddle/app/weather/models/WthWeatherDetails.java
+++ weather/src/main/java/com/oddle/app/weather/models/WthWeatherDetails.java
@@ -1,24 +1,15 @@
 package com.oddle.app.weather.models;
 
 import java.io.Serializable;
-import java.util.Date;
-
-import javax.persistence.CascadeType;
+import java.time.LocalDate;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.MapsId;
-import javax.persistence.OneToOne;
 import javax.persistence.Table;
-import javax.validation.constraints.NotEmpty;
 
 import lombok.Data;
-
 @Data
 @Entity
 @Table(name = "wth_weather_details")
@@ -32,26 +23,36 @@ public class WthWeatherDetails implements Serializable {
     private long id;
 
     @Column(name = "weather_dt", nullable = true)  
-    private Date weatherDt;
+    private LocalDate weatherDt;
 
     @Column(name = "city_id", nullable = true)  
-    private Double cityId;
+    private long cityId;
 
-    @Column(name = "weather_base_id", nullable = true)  
-    private Double WeatherBaseId;
+    @Column(name = "weather_base_id")  
+    private long weatherBaseId;
+
+    // @ManyToOne(fetch = FetchType.LAZY)
+	// @JoinColumn(name = "weather_base_id", insertable = false, updatable = false)
+	// @Fetch(FetchMode.JOIN)
+	// private WthWeatherBase weatherBase;
 
     @Column(name = "temperature", nullable = true)  
-    private Double temperature;
+    private double temperature;
 
     @Column(name = "temp_min", nullable = true)  
-    private Double tempMin;
+    private double tempMin;
     
     @Column(name = "temp_max", nullable = true)  
-    private Double tempMax;
+    private double tempMax;
 
     @Column(name = "pressure", nullable = true)  
-    private Double pressure;
+    private double pressure;
 
     @Column(name = "humidity", nullable = true)  
-    private Double humidity;
+    private double humidity;
+
+    // @ManyToOne(fetch = FetchType.LAZY)
+	// @JoinColumn(name = "city_id", insertable = false, updatable = false)
+	// @Fetch(FetchMode.JOIN)
+	// private WthCity city;
 }
