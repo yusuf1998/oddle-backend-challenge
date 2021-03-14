29a222afdc3dcb2263c534b8d6a95866da6f5b65
Yusuf Habibi <yusufhabibi1998@gmail.com>
2021-03-14 15:51:10 +0700

    delete not used library

--- weather/src/main/java/com/oddle/app/weather/models/WthWeatherBase.java
+++ weather/src/main/java/com/oddle/app/weather/models/WthWeatherBase.java
@@ -1,19 +1,12 @@
 package com.oddle.app.weather.models;
 
 import java.io.Serializable;
-import java.util.Date;
-import java.util.Set;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
-import javax.websocket.Encoder.Text;
-
 import lombok.Data;
 
 @Data
